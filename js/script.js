0ce50ecf4d418b5723a77b5842d152d84532d2e1
kalbek <access2kaleb@gmail.com>
2023-03-06 23:50:19 +0300

    correct indentation in script linters

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 function main() {
-    const openMenu = document.getElementById('open-mobile-menu');
-    const closeMenu = document.getElementById('close-mobile-menu');
-    const mobilePortfolio = document.getElementById('mobile-portfolio');
-    const mobileAbout = document.getElementById('mobile-about');
-    const mobileContact = document.getElementById('mobile-contact');
+  const openMenu = document.getElementById('open-mobile-menu');
+  const closeMenu = document.getElementById('close-mobile-menu');
+  const mobilePortfolio = document.getElementById('mobile-portfolio');
+  const mobileAbout = document.getElementById('mobile-about');
+  const mobileContact = document.getElementById('mobile-contact');
   
-    function closeMobileMenu() {
-      const mobileMenu = document.querySelector('#mobile-menu-active');
-      mobileMenu.classList.remove('visible');
-      mobileMenu.classList.add('hidden');
-    }
+  function closeMobileMenu() {
+    const mobileMenu = document.querySelector('#mobile-menu-active');
+    mobileMenu.classList.remove('visible');
+    mobileMenu.classList.add('hidden');
+  }
   
-    openMenu.addEventListener('click', () => {
-      const mobileMenu = document.querySelector('#mobile-menu-active');
-      mobileMenu.classList.remove('hidden');
-      mobileMenu.classList.add('visible', 'animate-from-top');
-    });
+  openMenu.addEventListener('click', () => {
+    const mobileMenu = document.querySelector('#mobile-menu-active');
+    mobileMenu.classList.remove('hidden');
+    mobileMenu.classList.add('visible', 'animate-from-top');
+  });
   
-    closeMenu.addEventListener('click', () => {
-      closeMobileMenu();
-    });
+  closeMenu.addEventListener('click', () => {
+    closeMobileMenu();
+  });
   
-    mobilePortfolio.addEventListener('click', () => {
-      closeMobileMenu();
-    });
+  mobilePortfolio.addEventListener('click', () => {
+    closeMobileMenu();
+  });
   
-    mobileAbout.addEventListener('click', () => {
-      closeMobileMenu();
-    });
+  mobileAbout.addEventListener('click', () => {
+    closeMobileMenu();
+  });
   
-    mobileContact.addEventListener('click', () => {
-      closeMobileMenu();
-    });
+  mobileContact.addEventListener('click', () => {
+    closeMobileMenu();
+  });
   }
   main();
   
